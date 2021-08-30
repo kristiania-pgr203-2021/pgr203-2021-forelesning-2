db5aa2d41397cb63377208ff0c93891c5beace74
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2021-08-30 15:57:58 +0200

    Create main method. Resolves #3

---
+++ roman/src/main/java/no/kristiania/RomanNumbers.java
@@ -0,0 +1,16 @@
+package no.kristiania;
+
+import java.util.Scanner;
+
+public class RomanNumbers {
+
+    public static void main(String[] args) {
+        System.out.println("Which number do you want in roman?");
+        
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        System.out.printf("%d in roman notation is %s", number,
+                "I don't know");
+    }
+    
+}
