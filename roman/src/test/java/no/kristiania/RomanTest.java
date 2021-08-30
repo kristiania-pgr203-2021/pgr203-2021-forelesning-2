025582e7f7f1a621326283aed7cab2c5db39301f
Øydis Kind Refsum <oeydis@gmail.com>
2021-08-30 16:35:58 +0200

    shouldTranslateOneToI(failing)

--- roman/src/test/java/no/kristiania/RomanTest.java
+++ roman/src/test/java/no/kristiania/RomanTest.java
@@ -1,4 +1,18 @@
 package no.kristiania;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class RomanTest {
+
+    @Test
+    void shouldTranslateOneToI() {
+        assertEquals("I", toRoman(1));
+    }
+
+    private String toRoman(int i) {
+        return null;
+    }
+
 }
